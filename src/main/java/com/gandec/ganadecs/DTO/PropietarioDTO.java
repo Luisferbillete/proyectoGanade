74a6fed923e1213dd41121d17fbc907235105ac8
Luisferbillete <luisfernandovllamil@gmail.com>
2023-06-22 19:07:40 -0500

    dto y update

---
+++ src/main/java/com/gandec/ganadecs/DTO/PropietarioDTO.java
@@ -0,0 +1,18 @@
+package com.gandec.ganadecs.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PropietarioDTO {
+    Integer  id;
+    String nombres;
+    String apellidos;
+    String direccion;
+    Integer telefono;
+
+
+}
