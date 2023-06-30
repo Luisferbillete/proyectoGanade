587c63f1f26a6b2cd056d6f866f15c9876690df3
Luisferbillete <luisfernandovllamil@gmail.com>
2023-06-29 21:12:42 -0500

    MapList,PropietaryDTO,y convertidor dto y convertidor entity

--- src/main/java/com/gandec/ganadecs/Services/PropietarioService.java
+++ src/main/java/com/gandec/ganadecs/Services/PropietarioService.java
@@ -1,11 +1,7 @@
 package com.gandec.ganadecs.Services;
 
-import com.gandec.ganadecs.DTO.PropietarioDTO;
-import com.gandec.ganadecs.DTO.PropietarioResponse;
 import com.gandec.ganadecs.DTO.PropietaryDTO;
-import com.gandec.ganadecs.Entity.Propietario;
 
-import java.security.PublicKey;
 import java.util.List;
 
 public interface PropietarioService {
