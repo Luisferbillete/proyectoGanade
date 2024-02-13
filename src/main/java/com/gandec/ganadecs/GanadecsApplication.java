eb56a685dda55f63405611f13235a48a52fae9b4
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-02-12 21:05:33 -0500

    cambios en user por propietarios

--- src/main/java/com/gandec/ganadecs/GanadecsApplication.java
+++ src/main/java/com/gandec/ganadecs/GanadecsApplication.java
@@ -2,12 +2,10 @@ package com.gandec.ganadecs;
 
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
