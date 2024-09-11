5db356493f82f6b8bce87a3913ffeb7fe7721a85
Luisferbillete <luisfernandovllamil@gmail.com>
2024-09-10 20:49:04 -0500

    Added: categoria y potrero en bovinorepository

--- src/main/java/com/gandec/ganadecs/Entity/Bovino.java
+++ src/main/java/com/gandec/ganadecs/Entity/Bovino.java
@@ -2,7 +2,6 @@ package com.gandec.ganadecs.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
